e4cc500af7e104a458666b5fcae63d918e13273c
Simon Schmidt <simon.schmidt@htp-tel.de>
2019-07-11 07:36:15 +0200

    bcns

--- ciphersuite2/bcns/bcns.go
+++ ciphersuite2/bcns/bcns.go
@@ -22,19 +22,19 @@ SOFTWARE.
 
 
 /*
-This package implements support for newhope, a key exchange based on the Ring Learning With Errors Problem.
+This package implements support for bcns, a key exchange based on the Ring Learning With Errors Problem.
 
 	PK_Algo = (
-		"newhope"
+		"bcns"
 	)
 */
-package newhope
+package bcns
 
 import (
 	"github.com/mad-day/cryptoinfra/ciphersuite2"
 	"github.com/mad-day/cryptoinfra/ciphersuite2/stretch"
 	
-	"github.com/mad-day/newhope"
+	"github.com/mad-day/Yawning-crypto/bcns"
 	"io"
 )
 
@@ -42,43 +42,62 @@ type pka_driver struct {}
 
 func (*pka_driver) GenerateKeyPair(rand io.Reader) (pub,priv []byte,err error) {
 	
-	epriv,epub,err2 := newhope.GenerateKeyPair(rand)
+	epriv,epub,err2 := bcns.GenerateKeyPair(rand)
 	if err2!=nil { err=err2; return }
-	priv = epriv.GetBytes(nil)
-	pub = epub.Send[:]
+	priv = epriv.Bytes()
+	pub = epub.Bytes()
 	
 	return
 }
 func (*pka_driver) LoadPublic(pub []byte) (ciphersuite2.PublicKey,error) {
-	if len(pub)!=newhope.SendASize { return nil,ciphersuite2.MalformedKeyError("NewHope-PublicKey") }
-	k := new(newhope.PublicKeyAlice)
-	copy(k.Send[:],pub)
+	k := new(bcns.PublicKey)
+	err := k.FromBytes(pub)
+	if err!=nil { return nil,err }
 	return k,nil
 }
 func (*pka_driver) LoadPrivate(priv []byte) (ciphersuite2.PrivateKey,error) {
-	k := new(newhope.PrivateKeyAlice)
-	if !k.SetBytes(priv) {
-		return nil,ciphersuite2.MalformedKeyError("NewHope-PrivateKey")
-	}
+	k := new(bcns.PrivateKey)
+	err := k.FromBytes(priv)
+	if err!=nil { return nil,err }
 	return k,nil
 }
 func (*pka_driver) DecryptKey(opaque []byte,prik ciphersuite2.PrivateKey,cb *ciphersuite2.Cipher_Buffer) error {
-	rp,ok := prik.(*newhope.PrivateKeyAlice)
-	if !ok { return ciphersuite2.InvalidKeyError("Expected *newhope.PrivateKeyAlice") }
-	bob := new(newhope.PublicKeyBob)
-	copy(bob.Send[:],opaque)
-	shared,err := newhope.KeyExchangeAlice(bob,rp)
+	rp,ok := prik.(*bcns.PrivateKey)
+	if !ok { return ciphersuite2.InvalidKeyError("Expected *bcns.PrivateKey") }
+	
+	Tp := new(bcns.PublicKey)
+	rec := new(bcns.RecData)
+	
+	var oprec []byte
+	
+	if len(opaque)>bcns.PublicKeySize {
+		opaque,oprec = opaque[:bcns.PublicKeySize],opaque[bcns.PublicKeySize:]
+	}
+	
+	err := Tp.FromBytes(opaque)
 	if err!=nil { return err }
+	err = rec.FromBytes(oprec)
+	if err!=nil { return err }
+	
+	shared := bcns.KeyExchangeAlice(Tp,rp,rec)
+	
 	stretch.DeriveKey(shared,cb)
 	return nil
 }
 func (*pka_driver) EncryptKey(rand io.Reader,pubk ciphersuite2.PublicKey,cb *ciphersuite2.Cipher_Buffer) (opaque []byte,err error) {
-	rp,ok := pubk.(*newhope.PublicKeyAlice)
-	if !ok { err = ciphersuite2.InvalidKeyError("Expected *newhope.PublicKeyAlice"); return }
+	rp,ok := pubk.(*bcns.PublicKey)
+	if !ok { err = ciphersuite2.InvalidKeyError("Expected *bcns.PublicKey"); return }
 	
-	resp,shared,err2 := newhope.KeyExchangeBob(rand,rp)
+	Ts,Tp,err2 := bcns.GenerateKeyPair(rand)
 	if err2!=nil { err = err2; return }
-	opaque = resp.Send[:]
+	
+	rec,shared,err2 := bcns.KeyExchangeBob(rand,rp,Ts)
+	if err2!=nil { err = err2; return }
+	
+	opaque = make([]byte,bcns.PublicKeySize+bcns.RecDataSize)
+	copy(opaque[:bcns.PublicKeySize],Tp.Bytes())
+	copy(opaque[bcns.PublicKeySize:],rec.Bytes())
+	
 	stretch.DeriveKey(shared,cb)
 	
 	return
@@ -87,6 +106,6 @@ func (*pka_driver) EncryptKey(rand io.Reader,pubk ciphersuite2.PublicKey,cb *cip
 var _ ciphersuite2.Pka_Driver = (*pka_driver)(nil)
 
 func init(){
-	ciphersuite2.RegisterPkAlgo("newhope",new(pka_driver))
+	ciphersuite2.RegisterPkAlgo("bcns",new(pka_driver))
 }
 
